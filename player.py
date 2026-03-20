41ae30c7873dccb1cf3588ce2a25afedd3720ef4
Jenny <Jenniferrbian@gmail.com>
2026-03-20 17:59:51 -0400

    player methods like move, get current position, and reset

--- player.py
+++ player.py
@@ -24,3 +24,19 @@ class Player:
     def get_path(self) -> list[int]:
         """Return the path this player has traversed in chronological order."""
         return self._path
+    
+    def move(self, position: int) -> None:
+        """Move the player to the given position and update the path and move count."""
+        self._path.append(position)
+        self._num_moves += 1
+
+    def get_current_position(self) -> int:
+        """Return the current position of the player, or -1 if the player has not moved yet."""
+        if self._path == []:
+            return -1
+        return self._path[-1]
+
+    def reset(self) -> None:
+        """Reset the player to their initial state."""
+        self._num_moves = 0
+        self._path = []
